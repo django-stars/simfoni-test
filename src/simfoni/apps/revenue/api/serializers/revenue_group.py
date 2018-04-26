26c72609abbfd499e39e0fcc6702cfa2ef54c0c3
Gleb Pushkov <gleptor@gmail.com>
2018-04-26 13:41:13 +0300

    Added default values from db when revenue is uploaded

--- src/simfoni/apps/revenue/api/serializers/revenue_group.py
+++ src/simfoni/apps/revenue/api/serializers/revenue_group.py
@@ -1,6 +1,8 @@
+from django.db.models import Min, Max
 from rest_framework import serializers
+from rest_framework.fields import CreateOnlyDefault
 
-from revenue.models import RevenueGroup
+from revenue.models import RevenueGroup, Revenue
 
 
 class RevenueGroupSerializer(serializers.ModelSerializer):
@@ -9,7 +11,24 @@ class RevenueGroupSerializer(serializers.ModelSerializer):
         model = RevenueGroup
         fields = '__all__'
 
+        # CreateOnlyDefault will be provided on object creation if they're not provided by FE side
+        extra_kwargs = {
+            'revenue_from': {
+                'default': CreateOnlyDefault(lambda: Revenue.objects.aggregate(min=Min('revenue'))['min']),
+            },
+            'revenue_to': {
+                'default': CreateOnlyDefault(lambda: Revenue.objects.aggregate(max=Max('revenue'))['max']),
+            },
+        }
+
     def validate(self, data):
+        # when data is not uploaded, the CreateOnlyDefault will return None.
+        # In this case we need to trigger required error for field
+        if data['revenue_from'] is None:
+            raise serializers.ValidationError({'revenue_from': [self.fields['revenue_from'].error_messages['required']]})
+        if data['revenue_to'] is None:
+            raise serializers.ValidationError({'revenue_to': [self.fields['revenue_to'].error_messages['required']]})
+
         if data['revenue_from'] >= data['revenue_to']:
-            raise serializers.ValidationError("Invalid range")
+            raise serializers.ValidationError(_("Invalid range"))
         return data
