824abb3a84650c54f10aa4703e84a7919f1961e8
alexgul4enko <alexgul4enko@mail.ru>
2018-05-11 13:43:09 +0300

    loaders

--- client/src/app/pages/dashboard/Matches.jsx
+++ client/src/app/pages/dashboard/Matches.jsx
@@ -1,6 +1,7 @@
 import { PureComponent, Fragment } from 'react'
 import { Col, Button } from 'reactstrap'
 import SortableTable, { BooleanColumn, Actions, Score } from 'common/widgets/SortableTable'
+import Loader from 'react-loader-spinner'
 import { autobind } from 'core-decorators'
 import get from 'lodash/get'
 import isEmpty from 'lodash/isEmpty'
@@ -37,34 +38,51 @@ const headers = [
 
 export default class Matches extends PureComponent {
 
+  constructor(props){
+    super(props)
+    this.state = {
+      loading: 0
+    }
+  }
+
   @autobind
   accept(companies=[]) {
-    return () => this.props.currentMatch.update(companies.map(item=>({...item, is_accepted: true})), { multiple: true, endpoint: 'matches/:uuid?' })
-      .then( _ => {
-        this.props.companies.fetch()
-        this.props.matches.fetch().then(resp => {
-          if(!resp || !resp.length) {
-            this.props.navigate('root')
-          }
-        })
-        .catch(error => {
-          this.props.navigate('root')
-        })
+    return () => {
+      this.setState({loading: 1}, ()=>{
+        this.props.currentMatch.update(companies.map(item=>({...item, is_accepted: true})), { multiple: true, endpoint: 'matches/:uuid?' })
+          .then( _ => {
+            this.setState({loading: 0})
+            this.props.companies.fetch()
+            this.props.matches.fetch().then(resp => {
+              if(!resp || !resp.length) {
+                this.props.navigate('root')
+              }
+            })
+            .catch(error => {
+              this.props.navigate('root')
+            })
+          })
       })
+    }
   }
 
   @autobind
   decline(companies=[]) {
-    return () => this.props.currentMatch.remove(companies, { multiple: true, endpoint: 'matches/:uuid?' })
-      .then( _ => {
-        this.props.companies.fetch().then(comp => {
-          if(comp.find(item=> item.uuid === this.props.match.params.uuid)) {
-            this.props.matches.fetch()
-          } else {
-            this.props.navigate('root')
-          }
-        })
+    return () => {
+      this.setState({loading: -1}, ()=>{
+        this.props.currentMatch.remove(companies, { multiple: true, endpoint: 'matches/:uuid?' })
+          .then( _ => {
+            this.setState({loading: 0})
+            this.props.companies.fetch().then(comp => {
+              if(comp.find(item=> item.uuid === this.props.match.params.uuid)) {
+                this.props.matches.fetch()
+              } else {
+                this.props.navigate('root')
+              }
+            })
+          })
       })
+    }
   }
 
 
@@ -79,12 +97,23 @@ export default class Matches extends PureComponent {
             joinToName='actions'
             accept={this.accept}
             decline={this.decline}
+            defaultSortKey="score"
           />
         </div>
         <footer>
           Apply changes to all matched companies <i className="material-icons">arrow_forward</i>
-          <Button disabled={isEmpty(get(this.props, 'matches.data', []))} color="success" onClick={this.accept(get(this.props, 'matches.data', []))}>Accept All</Button>
-          <Button disabled={isEmpty(get(this.props, 'matches.data', []))} color="danger" onClick={this.decline(get(this.props, 'matches.data', []))}>Decline All</Button>
+          <Button
+            disabled={isEmpty(get(this.props, 'matches.data', [])) || !!this.state.loading}
+            color="success" onClick={this.accept(get(this.props, 'matches.data', []))}
+          >
+            Accept All{this.state.loading === 1 && <Loader type='Oval' color='#fff' height='14' width='14' />}
+          </Button>
+          <Button
+            disabled={isEmpty(get(this.props, 'matches.data', [])) || !!this.state.loading}
+            color="danger" onClick={this.decline(get(this.props, 'matches.data', []))}
+          >
+            Decline All{this.state.loading === -1 && <Loader type='Oval' color='#fff' height='14' width='14' />}
+          </Button>
         </footer>
       </div>
     )
